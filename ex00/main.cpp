bf628c5bde8a8c2fc17d7337e20460e3f9892b13
emohamedd <a.elgaouej@gmail.com>
2023-10-30 11:14:00 +0100

    creating the serena my love fiels

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emohamed <emohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 10:30:07 by emohamed          #+#    #+#             */
-/*   Updated: 2023/10/30 10:46:11 by emohamed         ###   ########.fr       */
+/*   Updated: 2023/10/30 10:54:06 by emohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,7 @@ int main()
 {
     ClapTrap clap("emmd");
     clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
-    clap.attack("hassan");
     clap.takeDamage(20);
-    // clap.beRepaired(5);
+    clap.beRepaired(5);
     return (0);
 }
